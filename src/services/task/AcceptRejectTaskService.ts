cc5b9685031006b3eb76870c662f8ab129c5d52b
Jose Gouveia <1211089@isep.ipp.pt>
2023-12-30 18:29:24 +0000

    Refactor AcceptRejectTaskService class

--- src/services/task/AcceptRejectTaskService.ts
+++ src/services/task/AcceptRejectTaskService.ts
@@ -1,25 +1,23 @@
 import { Inject, Service } from "typedi";
-import  config  from "../../../config";
-import ITaskRepo from "../IRepos/ITaskRepo";
 import { IAcceptRejectTaskService } from "../IServices/task/IAcceptRejectTaskService";
 import { Result } from "../../core/logic/Result";
 import { AcceptRejectTaskDTO } from "../../dto/AcceptRejectTaskDTO";
+import ITaskRepo from "../IRepos/ITaskRepo";
+import config from "../../../config";
 
 @Service()
-export class AcceptRejectTaskService implements IAcceptRejectTaskService {
+export default class AcceptRejectTaskService implements IAcceptRejectTaskService {
 
     constructor(
-        @Inject(config.repos.task.name) private taskRepo: ITaskRepo,
+        @Inject(config.repos.task.name) private taskRepo: ITaskRepo
     ) { }
-    
+
     public async acceptOrRejectTask(dto: AcceptRejectTaskDTO): Promise<Result<boolean>> {
-        const task = await this.taskRepo.findById(dto.taskID)
-        if (task === null) {
-            return Result.fail<boolean>("Task not found")
-        }
-        dto.accept ? task.acceptTask() : task.rejectTask()
-        await this.taskRepo.save(task)
+        const task = await this.taskRepo.findById(dto.taskID);
+        if (task === null) return Result.fail<boolean>("Task not found");
+        dto.accept ? task.acceptTask() : task.rejectTask();
         return Result.ok<boolean>(true)
     }
+
 
 }
