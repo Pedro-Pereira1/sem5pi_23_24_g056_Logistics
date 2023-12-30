50d1f5ad7405b9187e724ad1200c2ef01a9b9888
Jose Gouveia <1211089@isep.ipp.pt>
2023-12-30 16:39:07 +0000

    Refactor TaskState class to use static readonly properties for state values

--- src/domain/TaskState.ts
+++ src/domain/TaskState.ts
@@ -2,27 +2,31 @@ import { initial } from 'lodash';
 import { ValueObject } from '../core/domain/ValueObject';
 import { Result } from '../core/logic/Result';
 
-  
-  interface TaskStateProps {
-    state: string;
+
+interface TaskStateProps {
+  state: string;
+}
+
+export class TaskState extends ValueObject<TaskStateProps> {
+
+  public static readonly Pending: string = "Pending"
+  public static readonly Accepted: string = "Accepted"
+  public static readonly Rejected: string = "Rejected"
+
+  constructor(props: TaskStateProps) {
+    super(props);
   }
 
-  export class TaskState extends ValueObject<TaskStateProps> {
-
-    constructor (props : TaskStateProps){
-      super(props);
-    }
-    
-    get state(): string {
-      return this.props.state
-    }
-    
-    public static create(): Result<TaskState> {
-        return Result.ok<TaskState>(new TaskState({state:"Pending"}))
-    }
-
-    public static createFromExisting(state: string): Result<TaskState> {
-      return Result.ok<TaskState>(new TaskState({state:state}))
-    }
-
+  get state(): string {
+    return this.props.state
   }
+
+  public static create(): Result<TaskState> {
+    return Result.ok<TaskState>(new TaskState({ state: TaskState.Pending }))
+  }
+
+  public static createFromExisting(state: string): Result<TaskState> {
+    return Result.ok<TaskState>(new TaskState({ state: state }))
+  }
+
+}
