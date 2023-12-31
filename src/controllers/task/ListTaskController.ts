fc45efab1f1c2a9a41829af62e3d1a49d82f988e
Alexandre Geração <1211151@isep.ipp.pt>
2023-12-31 17:35:47 +0000

    SC-45 Added listPendingTasks method to controller class

--- src/controllers/task/ListTaskController.ts
+++ src/controllers/task/ListTaskController.ts
@@ -4,15 +4,18 @@ import config from "../../../config";
 import { Result } from "../../core/logic/Result";
 import IListTaskController from "../IControllers/task/IListTaskController";
 import { IAuthService } from "../../services/IServices/auth/IAuthService";
+import IListPendingTaskService from "../../services/IServices/task/IListPendingTaskService";
+import ITaskDTO from "../../dto/TaskDTO";
 
 
 @Service()
 export default class ListTaskController implements IListTaskController {
-    
+
     constructor(
         @Inject(config.services.auth.name) private authService: IAuthService,
         @Inject(config.services.listAllTasks.name) private listAllTaskService,
-    ) 
+        @Inject(config.services.listTask.name) private taskService: IListPendingTaskService
+    )
     {}
 
     public async listTasksByParameter(req: Request, res: Response, next: NextFunction) {
@@ -42,7 +45,7 @@ export default class ListTaskController implements IListTaskController {
     }
 
 
-    public async listNotApprovedTasks(req: Request, res: Response, next: NextFunction) {
+    public async listPendingTasks(req: Request, res: Response, next: NextFunction) {
         if(!this.authService.validateToken(req)){
             return res.status(401).send("Unauthorized");
         }
@@ -54,14 +57,14 @@ export default class ListTaskController implements IListTaskController {
         }
 
         try {
-            //const buildingOrError = await this.service.createBuilding(req.body as IBuildingDTO) as Result<IBuildingDTO>
-        //
-            //if (buildingOrError.isFailure) {
-            //    return res.status(400).send(buildingOrError.errorValue())
-            //}
-            //
-            //const buildingDTO = buildingOrError.getValue();
-            //return res.status(201).json(buildingDTO);
+            const taskOrError = await this.taskService.listPendingTasks()
+
+            if (taskOrError.isFailure) {
+                return res.status(400).send()
+            }
+
+            const taskDTO = taskOrError.getValue();
+            return res.status(200).json(taskDTO);
 
         }catch (e){
             return next(e);
@@ -88,7 +91,7 @@ export default class ListTaskController implements IListTaskController {
 
             return res.status(200).json(tasks.getValue())
         }catch (e){
-            return next(e);
+          throw e;
         }
     }
 }
